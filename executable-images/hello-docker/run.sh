7ce771c5cbe9ea7b0778093358f9172bf9ac21f4
Chris Smith <chrisronsmith@gmail.com>
2021-03-27 14:55:58 -0400

    Nuke hello-dock

--- executable-images/hello-docker/run.sh
+++
@@ -1,8 +0,0 @@
-docker container run \
-    --rm \
-    --detach \
-    --publish 3000:3000 \
-    --name hd \
-    --volume $(pwd)/hello-dock:/home/node/app \
-    --volume /home/node/app/node_modules \
-    hello-docker
